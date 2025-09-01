2890c5e39642a83ddc5ae419eeb59c75b338b152
amelia <102411849+thanhtran134@users.noreply.github.com>
2025-09-02 01:55:46 +0700

    Update config.py

---
+++ config.py
@@ -0,0 +1,36 @@
+LLM_CONFIG = {
+
+}
+
+
+MCP_SERVERS = [
+    {
+
+    },
+    {
+
+    },
+
+]
+
+
+MCP_SERVERS2 = [
+    {
+
+    },
+    {
+
+    },
+    {
+
+    }
+]
+
+BOTRAG_STAGE = {
+
+}
+
+HR_SERVER = {
+    "api_host": "http://localhost:3003"
+}
+
